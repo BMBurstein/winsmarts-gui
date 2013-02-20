61196009f75d69ab63d2518ee75ee0f8b284b3af
Elad <elad55@gmail.com>
2013-02-20 14:53:41 +0200

    Minor changes

--- Logger/frmMain.cs
+++ Logger/frmMain.cs
@@ -11,16 +11,16 @@ using System.Net;
 
 namespace Logger
 {
-	public partial class frmMain : Form
-	{
-		UdpClient udpc = new UdpClient(44557);
+    public partial class frmMain : Form
+    {
+        UdpClient udpc = new UdpClient(44557);
 
-		public frmMain()
-		{
-			InitializeComponent();
+        public frmMain()
+        {
+            InitializeComponent();
             this.tabViews.Dock = System.Windows.Forms.DockStyle.Bottom;
             this.tabViews.Size = new System.Drawing.Size(this.tabViews.Size.Width, this.Size.Height - 60);
-		}
+        }
 
         private void reset()
         {
@@ -28,109 +28,109 @@ namespace Logger
             //lsvTasks.Items.Clear();
             ganttChart.reset();
         }
-        
+
         private void ReceiveCallback(IAsyncResult ar)
-		{
-			IPEndPoint ipep = null;
-			var rcv = udpc.EndReceive(ar, ref ipep);
-			this.Invoke((MethodInvoker)(delegate
-			{
-				doStuff(Encoding.ASCII.GetString(rcv));
-			}));
-			udpc.BeginReceive(new AsyncCallback(ReceiveCallback), null);
-		}
+        {
+            IPEndPoint ipep = null;
+            var rcv = udpc.EndReceive(ar, ref ipep);
+            this.Invoke((MethodInvoker)(delegate
+            {
+                doStuff(Encoding.ASCII.GetString(rcv));
+            }));
+            udpc.BeginReceive(new AsyncCallback(ReceiveCallback), null);
+        }
 
-		private void frmMain_Load(object sender, EventArgs e)
-		{
-			udpc.BeginReceive(new AsyncCallback(ReceiveCallback), null);
-		}
+        private void frmMain_Load(object sender, EventArgs e)
+        {
+            udpc.BeginReceive(new AsyncCallback(ReceiveCallback), null);
+        }
 
-		// All log proccessing should be done here
-		private void doStuff(string s)
-		{
-			string[] parts = s.Split(';');
+        // All log proccessing should be done here
+        private void doStuff(string s)
+        {
+            string[] parts = s.Split(';');
 
-			switch (parts[0])
-			{
+            switch (parts[0])
+            {
                 case "Start":
                     reset();
                     break;
                 case "DeclareTask":
-					handleDeclareTask(parts);
-					break;
+                    handleDeclareTask(parts);
+                    break;
                 case "ContextSwitch":
-					handleContextSwitch(parts);
-					break;
-				default:
-					break;
-			}
-		}
+                    handleContextSwitch(parts);
+                    break;
+                default:
+                    break;
+            }
+        }
 
-		List<string[]> CSHistory = new List<string[]>();
-		private void handleContextSwitch(string[] parts)
-		{
-			ganttChart.addSlice(uint.Parse(parts[2]));
-		}
+        List<string[]> CSHistory = new List<string[]>();
+        private void handleContextSwitch(string[] parts)
+        {
+            ganttChart.addSlice(uint.Parse(parts[2]));
+        }
 
-		List<Task> tasks = new List<Task>();
-		private void handleDeclareTask(string[] parts)
-		{
-			Task task = new Task() { tid = uint.Parse(parts[2]), name = parts[3], priority = uint.Parse(parts[4]), state = TaskStates.READY };
-			tasks.Add(task);
-			ListViewItem item = new ListViewItem(parts.Skip(2).ToArray());
-			item.SubItems.Add("Ready");
-			lsvTasks.Items.Add(item);
-			ganttChart.addTask(task.tid, task.name);
-		}
+        List<Task> tasks = new List<Task>();
+        private void handleDeclareTask(string[] parts)
+        {
+            Task task = new Task() { tid = uint.Parse(parts[2]), name = parts[3], priority = uint.Parse(parts[4]), state = TaskStates.READY };
+            tasks.Add(task);
+            ListViewItem item = new ListViewItem(parts.Skip(2).ToArray());
+            item.SubItems.Add("Ready");
+            lsvTasks.Items.Add(item);
+            ganttChart.addTask(task.tid, task.name);
+        }
 
         private void Show_tabs_CheckedChanged(object sender, EventArgs e)
-       {
-           if (Show_tabs.Checked)
-           {
-               this.tabPage1.Controls.Add(this.lsvTasks);
-               this.lsvTasks.Location = new Point(this.lsvTasks.Location.X, this.lsvTasks.Location.Y - 20);
-               this.lsvTasks.Dock = System.Windows.Forms.DockStyle.Fill;
+        {
+            if (Show_tabs.Checked)
+            {
+                this.tabPage1.Controls.Add(this.lsvTasks);
+                this.lsvTasks.Location = new Point(this.lsvTasks.Location.X, this.lsvTasks.Location.Y - 20);
+                this.lsvTasks.Dock = System.Windows.Forms.DockStyle.Fill;
 
-               this.tabPage2.Controls.Add(this.ganttChart);
-               this.ganttChart.Location = new Point(this.Size.Width / 2, this.ganttChart.Location.Y - 20);
-               this.ganttChart.Dock = System.Windows.Forms.DockStyle.Fill;
-               
-               tabViews.Visible = true;
-           }
-           else
-           {
-               this.tabViews.Size = new System.Drawing.Size(this.tabViews.Size.Width, this.Size.Height - 60);
+                this.tabPage2.Controls.Add(this.ganttChart);
+                this.ganttChart.Location = new Point(this.Size.Width / 2, this.ganttChart.Location.Y - 20);
+                this.ganttChart.Dock = System.Windows.Forms.DockStyle.Fill;
 
-               this.Controls.Add(this.lsvTasks);
-               this.lsvTasks.Dock = System.Windows.Forms.DockStyle.None;
-               this.lsvTasks.Size = new System.Drawing.Size(this.Size.Width / 2 - 10, this.Size.Height - 60);
-               this.lsvTasks.Location = new Point(this.lsvTasks.Location.X, this.lsvTasks.Location.Y + 20);
-               
-               this.Controls.Add(this.ganttChart);
-               this.ganttChart.Dock = System.Windows.Forms.DockStyle.None;
-               this.ganttChart.Size = new System.Drawing.Size(this.Size.Width / 2 - 10, this.Size.Height -60);
-               this.ganttChart.Location = new Point(this.Size.Width / 2, this.ganttChart.Location.Y + 20);
+                tabViews.Visible = true;
+            }
+            else
+            {
+                this.tabViews.Size = new System.Drawing.Size(this.tabViews.Size.Width, this.Size.Height - 60);
 
-               tabViews.Visible = false;
-           }
-       }
+                this.Controls.Add(this.lsvTasks);
+                this.lsvTasks.Dock = System.Windows.Forms.DockStyle.None;
+                this.lsvTasks.Size = new System.Drawing.Size(this.Size.Width / 2 - 10, this.Size.Height - 60);
+                this.lsvTasks.Location = new Point(this.lsvTasks.Location.X, this.lsvTasks.Location.Y + 20);
+
+                this.Controls.Add(this.ganttChart);
+                this.ganttChart.Dock = System.Windows.Forms.DockStyle.None;
+                this.ganttChart.Size = new System.Drawing.Size(this.Size.Width / 2 - 10, this.Size.Height - 60);
+                this.ganttChart.Location = new Point(this.Size.Width / 2, this.ganttChart.Location.Y + 20);
+
+                tabViews.Visible = false;
+            }
+        }
 
     }
 
-	public enum TaskStates
-	{
-		READY,
-		NOT_ACTIVE,
-		SUSPENDED,
-		SLEEPING,
-		UNKNOWN
-	}
+    public enum TaskStates
+    {
+        READY,
+        NOT_ACTIVE,
+        SUSPENDED,
+        SLEEPING,
+        UNKNOWN
+    }
 
-	public class Task
-	{
-		public uint priority { get; set; }
-		public string name { get; set; }
-		public uint tid { get; set; }
-		public TaskStates state { get; set; }
-	}
+    public class Task
+    {
+        public uint priority { get; set; }
+        public string name { get; set; }
+        public uint tid { get; set; }
+        public TaskStates state { get; set; }
+    }
 }
