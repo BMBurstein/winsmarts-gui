607ab91f47b00b069ceb016c2bc9ba910b4bf060
Baruch Burstein <bmburstein@gmail.com>
2013-03-01 09:00:34 +0200

    Basic scrolling for Gantt chart

--- Logger/Gantt.cs
+++ Logger/Gantt.cs
@@ -39,7 +39,8 @@ namespace Logger
 				spans.Add(new GanttSpan() { taskIndex = index, start = sliceCount, duration = 5 });
 			}
 			sliceCount+=5;
-			Invalidate();
+			splt.Panel2.AutoScrollMinSize = new Size((int)sliceCount, 0);
+			splt.Panel2.Invalidate();
 		}
 
 		public Gantt()
@@ -52,24 +53,46 @@ namespace Logger
 		private StringFormat sf;
 		private void Gantt_Paint(object sender, PaintEventArgs e)
 		{
+			//Graphics g = e.Graphics;
+			//float left = Math.Max(Width / 8, 50);
+			//float rowHeight = this.Height / (tasks.Count + 1);
+			//RectangleF box = new RectangleF(0, 0, left, rowHeight);
+			//float endX = Width;
+
+			//for (int i = 0; i < tasks.Count; ++i)
+			//{
+			//    box.Y = rowHeight * i;
+			//    g.DrawString(tasks.Values[i].name, DefaultFont, Brushes.Black, box, sf);
+			//}
+
+			//for (int i = spans.Count - 1; i >= 0; --i)
+			//{
+			//    endX -= spans[i].duration;
+			//    if (endX <= left)
+			//    {
+			//        g.FillRectangle(tasks[spans[i].taskIndex].brush, left, rowHeight * tasks.IndexOfKey(spans[i].taskIndex) + rowHeight / 4, spans[i].duration - (left - endX), rowHeight / 2);
+			//        break;
+			//    }
+			//    g.FillRectangle(tasks[spans[i].taskIndex].brush, endX, rowHeight * tasks.IndexOfKey(spans[i].taskIndex) + rowHeight / 4, spans[i].duration, rowHeight / 2);
+			//}
+		}
+
+		private void splitContainer1_Panel2_Paint(object sender, PaintEventArgs e)
+		{
 			Graphics g = e.Graphics;
-			float left = Math.Max(Width / 8, 50);
-			float rowHeight = this.Height / (tasks.Count + 1);
-			RectangleF box = new RectangleF(0, 0, left, rowHeight);
-			float endX = Width;
-
-			for (int i = 0; i < tasks.Count; ++i)
-			{
-				box.Y = rowHeight * i;
-				g.DrawString(tasks.Values[i].name, DefaultFont, Brushes.Black, box, sf);
-			}
-
+			float endX = sliceCount;
+			float rowHeight = 50;
+			
+			g.TranslateTransform(splt.Panel2.AutoScrollPosition.X, splt.Panel2.AutoScrollPosition.Y);
 			for (int i = spans.Count - 1; i >= 0; --i)
 			{
+				//if (spans[i].start >= splt.Panel2.AutoScrollPosition.X + splt.Panel2.Width)
+				//	continue;
+
 				endX -= spans[i].duration;
-				if (endX <= left)
+				if (endX <= 0)
 				{
-					g.FillRectangle(tasks[spans[i].taskIndex].brush, left, rowHeight * tasks.IndexOfKey(spans[i].taskIndex) + rowHeight / 4, spans[i].duration - (left - endX), rowHeight / 2);
+					g.FillRectangle(tasks[spans[i].taskIndex].brush, 0, rowHeight * tasks.IndexOfKey(spans[i].taskIndex) + rowHeight / 4, spans[i].duration + endX, rowHeight / 2);
 					break;
 				}
 				g.FillRectangle(tasks[spans[i].taskIndex].brush, endX, rowHeight * tasks.IndexOfKey(spans[i].taskIndex) + rowHeight / 4, spans[i].duration, rowHeight / 2);
