def8bbd26189e8f6a6e68ac0226afc1795c19837
Baruch Burstein <bmburstein@gmail.com>
2013-03-01 09:00:34 +0200

    Basic scrolling for Gantt chart

--- Logger/Gantt.Designer.cs
+++ Logger/Gantt.Designer.cs
@@ -28,19 +28,41 @@
 		/// </summary>
 		private void InitializeComponent()
 		{
+			this.splt = new System.Windows.Forms.SplitContainer();
+			((System.ComponentModel.ISupportInitialize)(this.splt)).BeginInit();
+			this.splt.SuspendLayout();
 			this.SuspendLayout();
+			// 
+			// splt
+			// 
+			this.splt.Dock = System.Windows.Forms.DockStyle.Fill;
+			this.splt.Location = new System.Drawing.Point(0, 0);
+			this.splt.Name = "splt";
+			// 
+			// splt.Panel2
+			// 
+			this.splt.Panel2.AutoScroll = true;
+			this.splt.Panel2.Paint += new System.Windows.Forms.PaintEventHandler(this.splitContainer1_Panel2_Paint);
+			this.splt.Size = new System.Drawing.Size(288, 150);
+			this.splt.SplitterDistance = 96;
+			this.splt.TabIndex = 0;
 			// 
 			// Gantt
 			// 
 			this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
 			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+			this.Controls.Add(this.splt);
 			this.Name = "Gantt";
 			this.Size = new System.Drawing.Size(288, 150);
 			this.Paint += new System.Windows.Forms.PaintEventHandler(this.Gantt_Paint);
+			((System.ComponentModel.ISupportInitialize)(this.splt)).EndInit();
+			this.splt.ResumeLayout(false);
 			this.ResumeLayout(false);
 
 		}
 
 		#endregion
+
+		private System.Windows.Forms.SplitContainer splt;
 	}
 }
