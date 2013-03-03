3b0c57a255d3fddc7e760791d4880dd843586850
Baruch Burstein <bmburstein@gmail.com>
2013-03-03 22:45:23 +0200

    Added vertical scrolling

--- Logger/frmMain.Designer.cs
+++ Logger/frmMain.Designer.cs
@@ -28,123 +28,125 @@
 		/// </summary>
 		private void InitializeComponent()
 		{
-            this.tabViews = new System.Windows.Forms.TabControl();
-            this.tabTaskList = new System.Windows.Forms.TabPage();
-            this.lsvTasks = new System.Windows.Forms.ListView();
-            this.tid = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
-            this.name = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
-            this.priority = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
-            this.state = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
-            this.tabGantt = new System.Windows.Forms.TabPage();
-            this.Show_tabs = new System.Windows.Forms.CheckBox();
-            this.ganttChart = new Logger.Gantt();
-            this.tabViews.SuspendLayout();
-            this.tabTaskList.SuspendLayout();
-            this.tabGantt.SuspendLayout();
-            this.SuspendLayout();
-            // 
-            // tabViews
-            // 
-            this.tabViews.Controls.Add(this.tabTaskList);
-            this.tabViews.Controls.Add(this.tabGantt);
-            this.tabViews.Dock = System.Windows.Forms.DockStyle.Bottom;
-            this.tabViews.Location = new System.Drawing.Point(0, 26);
-            this.tabViews.Name = "tabViews";
-            this.tabViews.SelectedIndex = 0;
-            this.tabViews.Size = new System.Drawing.Size(616, 341);
-            this.tabViews.TabIndex = 1;
-            // 
-            // tabPage1
-            // 
-            this.tabTaskList.Controls.Add(this.lsvTasks);
-            this.tabTaskList.Location = new System.Drawing.Point(4, 22);
-            this.tabTaskList.Name = "tabPage1";
-            this.tabTaskList.Padding = new System.Windows.Forms.Padding(3);
-            this.tabTaskList.Size = new System.Drawing.Size(608, 315);
-            this.tabTaskList.TabIndex = 0;
-            this.tabTaskList.Text = "Task Manager";
-            this.tabTaskList.UseVisualStyleBackColor = true;
-            // 
-            // lsvTasks
-            // 
-            this.lsvTasks.Columns.AddRange(new System.Windows.Forms.ColumnHeader[] {
+			this.tabViews = new System.Windows.Forms.TabControl();
+			this.tabTaskList = new System.Windows.Forms.TabPage();
+			this.lsvTasks = new System.Windows.Forms.ListView();
+			this.tid = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+			this.name = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+			this.priority = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+			this.state = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+			this.tabGantt = new System.Windows.Forms.TabPage();
+			this.ganttChart = new Logger.Gantt();
+			this.Show_tabs = new System.Windows.Forms.CheckBox();
+			this.tabViews.SuspendLayout();
+			this.tabTaskList.SuspendLayout();
+			this.tabGantt.SuspendLayout();
+			this.SuspendLayout();
+			// 
+			// tabViews
+			// 
+			this.tabViews.Controls.Add(this.tabTaskList);
+			this.tabViews.Controls.Add(this.tabGantt);
+			this.tabViews.Dock = System.Windows.Forms.DockStyle.Bottom;
+			this.tabViews.Location = new System.Drawing.Point(0, 26);
+			this.tabViews.Name = "tabViews";
+			this.tabViews.SelectedIndex = 0;
+			this.tabViews.Size = new System.Drawing.Size(616, 335);
+			this.tabViews.TabIndex = 1;
+			// 
+			// tabTaskList
+			// 
+			this.tabTaskList.Controls.Add(this.lsvTasks);
+			this.tabTaskList.Location = new System.Drawing.Point(4, 22);
+			this.tabTaskList.Name = "tabTaskList";
+			this.tabTaskList.Padding = new System.Windows.Forms.Padding(3);
+			this.tabTaskList.Size = new System.Drawing.Size(608, 309);
+			this.tabTaskList.TabIndex = 0;
+			this.tabTaskList.Text = "Task Manager";
+			this.tabTaskList.UseVisualStyleBackColor = true;
+			// 
+			// lsvTasks
+			// 
+			this.lsvTasks.Columns.AddRange(new System.Windows.Forms.ColumnHeader[] {
             this.tid,
             this.name,
             this.priority,
             this.state});
-            this.lsvTasks.Dock = System.Windows.Forms.DockStyle.Fill;
-            this.lsvTasks.FullRowSelect = true;
-            this.lsvTasks.Location = new System.Drawing.Point(3, 3);
-            this.lsvTasks.MultiSelect = false;
-            this.lsvTasks.Name = "lsvTasks";
-            this.lsvTasks.Size = new System.Drawing.Size(602, 309);
-            this.lsvTasks.TabIndex = 0;
-            this.lsvTasks.UseCompatibleStateImageBehavior = false;
-            this.lsvTasks.View = System.Windows.Forms.View.Details;
-            // 
-            // tid
-            // 
-            this.tid.Text = "ID";
-            // 
-            // name
-            // 
-            this.name.Text = "Name";
-            // 
-            // priority
-            // 
-            this.priority.Text = "Priority";
-            // 
-            // state
-            // 
-            this.state.Text = "State";
-            // 
-            // tabPage2
-            // 
-            this.tabGantt.Controls.Add(this.ganttChart);
-            this.tabGantt.Location = new System.Drawing.Point(4, 22);
-            this.tabGantt.Name = "tabPage2";
-            this.tabGantt.Padding = new System.Windows.Forms.Padding(3);
-            this.tabGantt.Size = new System.Drawing.Size(608, 315);
-            this.tabGantt.TabIndex = 1;
-            this.tabGantt.Text = "Gantt Chart";
-            this.tabGantt.UseVisualStyleBackColor = true;
-            // 
-            // Show_tabs
-            // 
-            this.Show_tabs.AutoSize = true;
-            this.Show_tabs.Checked = true;
-            this.Show_tabs.CheckState = System.Windows.Forms.CheckState.Checked;
-            this.Show_tabs.Location = new System.Drawing.Point(0, 3);
-            this.Show_tabs.Name = "Show_tabs";
-            this.Show_tabs.Size = new System.Drawing.Size(76, 17);
-            this.Show_tabs.TabIndex = 2;
-            this.Show_tabs.Text = "Show tabs";
-            this.Show_tabs.UseVisualStyleBackColor = true;
-            this.Show_tabs.CheckedChanged += new System.EventHandler(this.Show_tabs_CheckedChanged);
-            // 
-            // ganttChart
-            // 
-            this.ganttChart.Dock = System.Windows.Forms.DockStyle.Fill;
-            this.ganttChart.Location = new System.Drawing.Point(3, 3);
-            this.ganttChart.Name = "ganttChart";
-            this.ganttChart.Size = new System.Drawing.Size(602, 309);
-            this.ganttChart.TabIndex = 0;
-            // 
-            // frmMain
-            // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
-            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(616, 367);
-            this.Controls.Add(this.Show_tabs);
-            this.Controls.Add(this.tabViews);
-            this.Name = "frmMain";
-            this.Text = "Logger Test";
-            this.Load += new System.EventHandler(this.frmMain_Load);
-            this.tabViews.ResumeLayout(false);
-            this.tabTaskList.ResumeLayout(false);
-            this.tabGantt.ResumeLayout(false);
-            this.ResumeLayout(false);
-            this.PerformLayout();
+			this.lsvTasks.Dock = System.Windows.Forms.DockStyle.Fill;
+			this.lsvTasks.FullRowSelect = true;
+			this.lsvTasks.Location = new System.Drawing.Point(3, 3);
+			this.lsvTasks.MultiSelect = false;
+			this.lsvTasks.Name = "lsvTasks";
+			this.lsvTasks.Size = new System.Drawing.Size(602, 303);
+			this.lsvTasks.TabIndex = 0;
+			this.lsvTasks.UseCompatibleStateImageBehavior = false;
+			this.lsvTasks.View = System.Windows.Forms.View.Details;
+			// 
+			// tid
+			// 
+			this.tid.Text = "ID";
+			// 
+			// name
+			// 
+			this.name.Text = "Name";
+			// 
+			// priority
+			// 
+			this.priority.Text = "Priority";
+			// 
+			// state
+			// 
+			this.state.Text = "State";
+			// 
+			// tabGantt
+			// 
+			this.tabGantt.Controls.Add(this.ganttChart);
+			this.tabGantt.Location = new System.Drawing.Point(4, 22);
+			this.tabGantt.Name = "tabGantt";
+			this.tabGantt.Padding = new System.Windows.Forms.Padding(3);
+			this.tabGantt.Size = new System.Drawing.Size(608, 309);
+			this.tabGantt.TabIndex = 1;
+			this.tabGantt.Text = "Gantt Chart";
+			this.tabGantt.UseVisualStyleBackColor = true;
+			// 
+			// ganttChart
+			// 
+			this.ganttChart.Dock = System.Windows.Forms.DockStyle.Fill;
+			this.ganttChart.Location = new System.Drawing.Point(3, 3);
+			this.ganttChart.Name = "ganttChart";
+			this.ganttChart.RowHeight = 50;
+			this.ganttChart.Size = new System.Drawing.Size(602, 303);
+			this.ganttChart.TabIndex = 0;
+			this.ganttChart.YScale = 5;
+			// 
+			// Show_tabs
+			// 
+			this.Show_tabs.AutoSize = true;
+			this.Show_tabs.Checked = true;
+			this.Show_tabs.CheckState = System.Windows.Forms.CheckState.Checked;
+			this.Show_tabs.Location = new System.Drawing.Point(0, 3);
+			this.Show_tabs.Name = "Show_tabs";
+			this.Show_tabs.Size = new System.Drawing.Size(76, 17);
+			this.Show_tabs.TabIndex = 2;
+			this.Show_tabs.Text = "Show tabs";
+			this.Show_tabs.UseVisualStyleBackColor = true;
+			this.Show_tabs.CheckedChanged += new System.EventHandler(this.Show_tabs_CheckedChanged);
+			// 
+			// frmMain
+			// 
+			this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+			this.ClientSize = new System.Drawing.Size(616, 361);
+			this.Controls.Add(this.Show_tabs);
+			this.Controls.Add(this.tabViews);
+			this.Name = "frmMain";
+			this.Text = "Logger Test";
+			this.Load += new System.EventHandler(this.frmMain_Load);
+			this.tabViews.ResumeLayout(false);
+			this.tabTaskList.ResumeLayout(false);
+			this.tabGantt.ResumeLayout(false);
+			this.ResumeLayout(false);
+			this.PerformLayout();
 
 		}
 
