ca9f2c2846180e7a28865c60650b1fde38468e10
Baruch Burstein <bmburstein@gmail.com>
2013-03-03 21:25:44 +0200

    Better scolling and painting

--- Logger/Gantt.cs
+++ Logger/Gantt.cs
@@ -12,9 +12,40 @@ namespace Logger
 	public partial class Gantt : UserControl
 	{
 		private List<GanttSpan> spans = new List<GanttSpan>();
-		//private List<string> names = new List<string>();
-		private SortedList<uint, GanttTask> tasks = new SortedList<uint, GanttTask>();
+		private Dictionary<uint, GanttTask> tasks = new Dictionary<uint, GanttTask>();
 		private uint sliceCount = 0;
+		private Panel chart = new BufferedPanel() { Dock = DockStyle.Fill, AutoScroll = true };
+		private int scale;
+		private int rowHeight;
+
+		public int RowHeight
+		{
+			get
+			{
+				return rowHeight;
+			}
+			set
+			{
+				rowHeight = value;
+				foreach (Control label in splt.Panel1.Controls)
+				{
+					label.Height = rowHeight;
+				}
+				Invalidate();
+			}
+		}
+		public int YScale
+		{
+			get
+			{
+				return scale;
+			}
+			set
+			{
+				scale = value;
+				Invalidate();
+			}
+		}
 
 		public void reset()
 		{
@@ -25,101 +56,90 @@ namespace Logger
 		
 		public void addTask(uint index, string name)
 		{
-			tasks.Add(index, new GanttTask(name));
+			tasks.Add(index, new GanttTask(index, name, tasks.Count()));
+			Label l = new Label()
+				{
+					Text = name,
+					Height = RowHeight,
+					Dock = DockStyle.Top,
+					TextAlign = ContentAlignment.MiddleRight
+				};
+			splt.Panel1.Controls.Add(l);
+			splt.Panel1.Controls.SetChildIndex(l, 0);
 		}
 
 		public void addSlice(uint index)
 		{
-			if (spans.Count > 0 && index == spans.Last().taskIndex)
+			if (spans.Count > 0 && index == spans.Last().task.index)
 			{
-				spans.Last().duration+=5;
+				spans.Last().duration++;
 			}
 			else
 			{
-				spans.Add(new GanttSpan() { taskIndex = index, start = sliceCount, duration = 5 });
+				spans.Add(new GanttSpan() { task = tasks[index], start = sliceCount, duration = 1 });
 			}
-			sliceCount+=5;
-			splt.Panel2.AutoScrollMinSize = new Size((int)sliceCount, 0);
-			splt.Panel2.Invalidate();
+			sliceCount++;
+			chart.Invalidate();
 		}
 
 		public Gantt()
 		{
 			InitializeComponent();
-			sf = new StringFormat();
-			sf.LineAlignment = StringAlignment.Center;
+			splt.Panel2.Controls.Add(chart);
+			chart.Paint += Chart_Paint;
+			RowHeight = 50;
+			YScale = 5;
 		}
 
-		private StringFormat sf;
-		private void Gantt_Paint(object sender, PaintEventArgs e)
-		{
-			//Graphics g = e.Graphics;
-			//float left = Math.Max(Width / 8, 50);
-			//float rowHeight = this.Height / (tasks.Count + 1);
-			//RectangleF box = new RectangleF(0, 0, left, rowHeight);
-			//float endX = Width;
-
-			//for (int i = 0; i < tasks.Count; ++i)
-			//{
-			//    box.Y = rowHeight * i;
-			//    g.DrawString(tasks.Values[i].name, DefaultFont, Brushes.Black, box, sf);
-			//}
-
-			//for (int i = spans.Count - 1; i >= 0; --i)
-			//{
-			//    endX -= spans[i].duration;
-			//    if (endX <= left)
-			//    {
-			//        g.FillRectangle(tasks[spans[i].taskIndex].brush, left, rowHeight * tasks.IndexOfKey(spans[i].taskIndex) + rowHeight / 4, spans[i].duration - (left - endX), rowHeight / 2);
-			//        break;
-			//    }
-			//    g.FillRectangle(tasks[spans[i].taskIndex].brush, endX, rowHeight * tasks.IndexOfKey(spans[i].taskIndex) + rowHeight / 4, spans[i].duration, rowHeight / 2);
-			//}
-		}
-
-		private void splitContainer1_Panel2_Paint(object sender, PaintEventArgs e)
+		private void Chart_Paint(object sender, PaintEventArgs e)
 		{
 			Graphics g = e.Graphics;
-			float endX = sliceCount;
-			float rowHeight = 50;
-			
-			g.TranslateTransform(splt.Panel2.AutoScrollPosition.X, splt.Panel2.AutoScrollPosition.Y);
-			for (int i = spans.Count - 1; i >= 0; --i)
-			{
-				//if (spans[i].start >= splt.Panel2.AutoScrollPosition.X + splt.Panel2.Width)
-				//	continue;
+			bool autoScroll = (chart.Width - chart.AutoScrollPosition.X >= chart.AutoScrollMinSize.Width);
+			chart.AutoScrollMinSize = new Size((int)sliceCount * scale, 0);
 
-				endX -= spans[i].duration;
-				if (endX <= 0)
-				{
-					g.FillRectangle(tasks[spans[i].taskIndex].brush, 0, rowHeight * tasks.IndexOfKey(spans[i].taskIndex) + rowHeight / 4, spans[i].duration + endX, rowHeight / 2);
-					break;
-				}
-				g.FillRectangle(tasks[spans[i].taskIndex].brush, endX, rowHeight * tasks.IndexOfKey(spans[i].taskIndex) + rowHeight / 4, spans[i].duration, rowHeight / 2);
+			g.TranslateTransform(chart.AutoScrollPosition.X, chart.AutoScrollPosition.Y);
+			foreach (var span in spans)
+			{
+				g.FillRectangle(span.task.brush, span.start * scale, RowHeight * span.task.order + RowHeight / 4, span.duration * scale, RowHeight / 2);
 			}
+
+			if (autoScroll)
+				chart.HorizontalScroll.Value = chart.HorizontalScroll.Maximum;
 		}
 
 		private class GanttTask
 		{
 			private static readonly Random rand = new Random();
+			public uint index { get; set; }
 			public string name { get; set; }
 			public Brush brush { get; set; }
-			public GanttTask(string name)
+			public int order { get; set; }
+			public GanttTask(uint index, string name, int order)
 			{
+				this.index = index;
 				this.name = name;
-				brush = new SolidBrush(Color.FromArgb(rand.Next(128), rand.Next(128), rand.Next(128)));
+				this.order = order;
+				brush = new SolidBrush(Color.FromArgb(rand.Next(192), rand.Next(192), rand.Next(192)));
 			}
 		}
 
 		private class GanttSpan : IComparable<GanttSpan>
 		{
-			public uint taskIndex { get; set; }
+			public GanttTask task { get; set; }
 			public float start { get; set; }
 			public float duration { get; set; }
 
 			public int CompareTo(GanttSpan other)
 			{
 				return start.CompareTo(other.start);
+			}
+		}
+
+		class BufferedPanel : Panel
+		{
+			public BufferedPanel()
+			{
+				this.DoubleBuffered = true;
 			}
 		}
 	}
